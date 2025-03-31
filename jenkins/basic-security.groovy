72deb528674af01a038050e5bf4e8ae5fb096835
Tariq Dwekat <tariqzdwekat@gmail.com>
2025-03-30 23:37:46 -0500

    jenkins pipeline fixes

---
+++ jenkins/basic-security.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount('admin', 'admin123')  // 👈 Set your custom password here
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
